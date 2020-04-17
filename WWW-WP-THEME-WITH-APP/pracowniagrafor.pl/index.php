db746231475bfe8b91d916ec19b5b2065119f7d1
Sebastian-Kr <kontakt@progremo.pl>
2020-04-17 11:42:38 +0200

    Add portfolio

--- WWW-WP-THEME-WITH-APP/pracowniagrafor.pl/index.php
+++ WWW-WP-THEME-WITH-APP/pracowniagrafor.pl/index.php
@@ -1,17 +1,32 @@
+
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+?>
+<?php get_header(); ?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<?php get_template_part( 'content', 'header' );?>
+
+<?php if (is_page(8)) {get_template_part('content', 'kursy_loop1');} ?>
+
+<section>
+
+  <div class="container"><div class="row"><div class="col-md-12">
+  <?php get_template_part( 'content', 'main' );?>
+  </div></div></div>
+
+
+<?php if (is_page(694)): ?>
+<div class="container"><div class="row"><div class="col-12-sm"> <?php echo do_shortcode('[htmlSitemap]') ?></div></div></div>
+<?php endif; ?>
+
+
+
+<?php if (get_field('progremo_text_nm1')): ?>
+  <div class="container mt40"><div class="row"><div class="col-md-12">
+  <?php ssacf('progremo_text_nm1') ?>
+  </div></div></div>
+<?php endif; ?>
+
+</section>
+
+<?php get_footer(); ?>
